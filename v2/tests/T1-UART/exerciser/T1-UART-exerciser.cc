f8ae5c60168f4e78de2ff0549b61277413d4558b
Adam Mayer <adam@makerbot.com>
2009-12-10 18:38:51 -0500

    Exerciser now just gives stats.  Twenty-five thousand packets and counting, yay us.

--- v2/tests/T1-UART/exerciser/T1-UART-exerciser.cc
+++ v2/tests/T1-UART/exerciser/T1-UART-exerciser.cc
@@ -55,8 +55,9 @@ int main()
 	OutPacket out;
 
 	uint16_t sequence_number = 0;
+	long long bytes_txd = 0;
 	// can't do 32, since we drop the debug command byte
-	while(sequence_number < 10) {
+	while(1) {
 		uint8_t length_in_bytes = random()%31;
 		sequence_number++;
 		// write a packet
@@ -66,12 +67,14 @@ int main()
 		for (int i =0; i < length_in_bytes; i++) {
 			uint8_t val = (i%2 == 0)?(sequence_number & 0xff):(sequence_number>>8);
 			out.append8(val);
+			bytes_txd++;
 		}
+		bytes_txd += 4;
 		while (!out.isFinished()) {
 			uint8_t out_byte = out.getNextByteToSend();
 			write(fd,&out_byte,1);
 		}
-		printf("Finished sending packet SN %d, length %d\n", sequence_number,length_in_bytes);
+//		printf("Finished sending packet SN %d, length %d\n", sequence_number,length_in_bytes);
 		while (!in.isFinished()) {
 			uint8_t in_byte;
 			int count = read(fd,&in_byte,1);
@@ -79,16 +82,23 @@ int main()
 				in.processByte(in_byte);
 			}
 		}
-		printf("Received packet SN %d\n", sequence_number);
+//		printf("Received packet SN %d\n", sequence_number);
 		// Verify
+		if (in.hasError()) {
+			fprintf(stderr,"%d - PACKET ERROR (error code %d)\n",sequence_number,in.getErrorCode());
+		}
 		if (!in.getLength() == length_in_bytes) {
-			fprintf(stderr,"LENGTH MISMATCH (%d, expected %d)\n", in.getLength(), length_in_bytes);
+			fprintf(stderr,"%d - LENGTH MISMATCH (%d, expected %d)\n",sequence_number, in.getLength(), length_in_bytes);
 		}
 		for (int i =0; i < length_in_bytes; i++) {
 			uint8_t val = (i%2 == 0)?(sequence_number & 0xff):(sequence_number>>8);
 			if (in.read8(i) != val) {
-				fprintf(stderr,"DATA MISMATCH on index %d (%d, expected %d)\n", i, in.read8(i), val);
+				fprintf(stderr,"%d - DATA MISMATCH on index %d (%d, expected %d)\n",sequence_number, i, in.read8(i), val);
 			}
+		}
+		if (sequence_number % 100 == 0) {
+			printf("Packet sequence: %u (%llu bytes transmitted)\n", sequence_number, bytes_txd);
+			fflush(stdout);
 		}
 	}
 
