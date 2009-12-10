0b7bf0c496dae7a25bd454591013003fefd24755
Adam Mayer <adam@makerbot.com>
2009-12-10 16:45:37 -0500

    uart test

--- v2/tests/T1-UART/fw/T1-UART.cc
+++ v2/tests/T1-UART/fw/T1-UART.cc
@@ -8,12 +8,18 @@
 #include "util/UART.hh"
 #include "util/DebugPacketProcessor.hh"
 
-int main()
-{
-	while(1) {
-		if (processDebugPacket(uart[0].in_, uart[0].out_))
-		{
-			uart[0].beginSend();
+int main() {
+	while (1) {
+		if (uart[0].in_.isFinished()) {
+			if (processDebugPacket(uart[0].in_, uart[0].out_)) {
+				uart[0].in_.reset();
+				uart[0].beginSend();
+			} else {
+				uart[0].in_.reset();
+			}
+			while (!uart[0].out_.isFinished())
+				;
+			uart[0].out_.reset();
 		}
 	}
 	return 0;
