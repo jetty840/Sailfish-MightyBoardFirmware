e2af4b0b3194e11ff04e8d75111b74e2250cef17
Adam Mayer <phooky@Spectro.(none)>
2010-03-08 13:07:23 -0500

    Extruder motor running on v2

--- v2/src/Extruder/boards/ecv22/ExtruderMotor.cc
+++ v2/src/Extruder/boards/ecv22/ExtruderMotor.cc
@@ -11,11 +11,12 @@
 
 // TIMER0 is used to PWM motor driver A enable on OC0B.
 void initExtruderMotor() {
-	TCCR0A = 0b00100011;
-	TCCR0B = 0b00000001;
+	TCCR0A = 0b00000011;  // Leave pin off by default
+	TCCR0B = 0b00000011;
 	OCR0B = 0;
 	TIMSK0 = 0; // no interrupts needed.
 	MOTOR_ENABLE_PIN.setDirection(true);
+	MOTOR_ENABLE_PIN.setValue(false);
 	MOTOR_DIR_PIN.setDirection(true);
 }
 
@@ -31,5 +32,5 @@ void setExtruderMotor(int16_t speed) {
 	if (backwards) { speed = -speed; }
 	if (speed > 255) { speed = 255; }
 	MOTOR_DIR_PIN.setValue(!backwards);
-	OCR0A = speed;
+	OCR0B = speed;
 }
