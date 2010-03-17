082a062c5eb8d0b828cba7cea8c297b2551a1c9c
Adam Mayer <phooky@Spectro.(none)>
2010-03-17 13:04:02 -0400

    remove lock block on tool queries.  This was the last tool blocker in the codebase, hooray.

--- v2/src/Motherboard/Host.cc
+++ v2/src/Motherboard/Host.cc
@@ -60,18 +60,18 @@ void runHostSlice() {
 	if (waiting_for_tool_response) {
 		// TODO
 		/*
-		if (isToolTransactionDone()) {
-			// return packet
-			// Copy payload back. Start from 0-- we need the response code.
-			for (int i = 0; i < getToolIn().getLength(); i++) {
-				out.append8(getToolIn().read8(i));
-			}
-			in.reset();
-			uart[0].beginSend();
-			releaseToolLock();
-		}
-		return;
-		*/
+		 if (isToolTransactionDone()) {
+		 // return packet
+		 // Copy payload back. Start from 0-- we need the response code.
+		 for (int i = 0; i < getToolIn().getLength(); i++) {
+		 out.append8(getToolIn().read8(i));
+		 }
+		 in.reset();
+		 uart[0].beginSend();
+		 releaseToolLock();
+		 }
+		 return;
+		 */
 	}
 	if (in.isStarted() && !in.isFinished()) {
 		if (!packet_in_timeout.isActive()) {
@@ -170,31 +170,37 @@ bool processQueryPacket(const InPacket& from_host, OutPacket& to_host) {
 				command::pause(!command::isPaused());
 				to_host.append8(RC_OK);
 				return true;
-			case HOST_CMD_TOOL_QUERY:
-				{
-					// FIXME: ADD TIMEOUT
-					while (!tool::getLock());
-					OutPacket& out = tool::getOutPacket();
-					InPacket& in = tool::getInPacket();
-					out.reset();
-					for (int i = 1; i < from_host.getLength(); i++) {
-						out.append8(from_host.read8(i));
-					}
-					tool::startTransaction();
-					while (!tool::isTransactionDone()) {
-						tool::runToolSlice();
-					}
-					if (in.getErrorCode() == PacketError::PACKET_TIMEOUT) {
+			case HOST_CMD_TOOL_QUERY: {
+				Timeout query_tool_timeout;
+				query_tool_timeout.start(HOST_TOOL_RESPONSE_TIMEOUT_MS);
+				while (!tool::getLock()) {
+					if (query_tool_timeout.hasElapsed()) {
 						to_host.append8(RC_DOWNSTREAM_TIMEOUT);
-						tool::releaseLock();
+						Motherboard::getBoard().indicateError(3);
 						return true;
 					}
+				}
+				OutPacket& out = tool::getOutPacket();
+				InPacket& in = tool::getInPacket();
+				out.reset();
+				for (int i = 1; i < from_host.getLength(); i++) {
+					out.append8(from_host.read8(i));
+				}
+				tool::startTransaction();
+				while (!tool::isTransactionDone()) {
+					tool::runToolSlice();
+				}
+				if (in.getErrorCode() == PacketError::PACKET_TIMEOUT) {
+					Motherboard::getBoard().indicateError(4);
+					to_host.append8(RC_DOWNSTREAM_TIMEOUT);
+				} else {
 					// Copy payload back. Start from 0-- we need the response code.
 					for (int i = 0; i < in.getLength(); i++) {
 						to_host.append8(in.read8(i));
 					}
-					tool::releaseLock();
 				}
+				tool::releaseLock();
+			}
 				return true;
 			case HOST_CMD_IS_FINISHED:
 				to_host.append8(RC_OK);
@@ -203,31 +209,29 @@ bool processQueryPacket(const InPacket& from_host, OutPacket& to_host) {
 					to_host.append8(done?1:0);
 				}
 				return true;
-			case HOST_CMD_READ_EEPROM:
-				{
-					uint16_t offset = from_host.read16(1);
-					uint8_t length = from_host.read8(3);
-					uint8_t data[16];
-					eeprom_read_block(data,(const void*)offset, length);
-					to_host.append8(RC_OK);
-					for (int i = 0; i < length; i++) {
-						to_host.append8(data[i]);
-					}
+			case HOST_CMD_READ_EEPROM: {
+				uint16_t offset = from_host.read16(1);
+				uint8_t length = from_host.read8(3);
+				uint8_t data[16];
+				eeprom_read_block(data, (const void*) offset, length);
+				to_host.append8(RC_OK);
+				for (int i = 0; i < length; i++) {
+					to_host.append8(data[i]);
 				}
+			}
 				return true;
-			case HOST_CMD_WRITE_EEPROM:
-				{
-					uint16_t offset = from_host.read16(1);
-					uint8_t length = from_host.read8(3);
-					uint8_t data[16];
-					eeprom_read_block(data,(const void*)offset, length);
-					for (int i = 0; i < length; i++) {
-						data[i] = from_host.read8(i+4);
-					}
-					eeprom_write_block(data,(void*)offset, length);
-					to_host.append8(RC_OK);
-					to_host.append8(length);
+			case HOST_CMD_WRITE_EEPROM: {
+				uint16_t offset = from_host.read16(1);
+				uint8_t length = from_host.read8(3);
+				uint8_t data[16];
+				eeprom_read_block(data, (const void*) offset, length);
+				for (int i = 0; i < length; i++) {
+					data[i] = from_host.read8(i + 4);
 				}
+				eeprom_write_block(data, (void*) offset, length);
+				to_host.append8(RC_OK);
+				to_host.append8(length);
+			}
 				return true;
 			}
 		}
