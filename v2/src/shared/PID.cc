32e3516ffcbac0f9edff055b7761eb4c7029ec3f
Adam Mayer <phooky@Spectro.(none)>
2010-03-23 13:13:17 -0400

    more rename/move/refactor

--- v2/src/shared/PID.cc
+++ v2/src/shared/PID.cc
@@ -11,26 +11,26 @@ const int ERR_ACC_MAX = 0xff;
 const int ERR_ACC_MIN = -0xff;
 
 void PID::reset() {
-	error_acc_ = 0;
-	prev_input_ = 0;
+	error_acc = 0;
+	prev_input = 0;
 }
 
 int PID::calculate(const int pv) {
-	int e = sp_ - pv;
-	error_acc_ += e;
+	int e = sp - pv;
+	error_acc += e;
 	// clamp the error accumulator at +-2**12
-	if (error_acc_ > ERR_ACC_MAX) {
-		error_acc_ = ERR_ACC_MAX;
+	if (error_acc > ERR_ACC_MAX) {
+		error_acc = ERR_ACC_MAX;
 	}
-	if (error_acc_ < ERR_ACC_MIN) {
-		error_acc_ = ERR_ACC_MIN;
+	if (error_acc < ERR_ACC_MIN) {
+		error_acc = ERR_ACC_MIN;
 	}
-	float p_term = e * p_gain_;
-	float i_term = error_acc_ * i_gain_;
-	int delta = pv - prev_input_;
-	float d_term = delta * d_gain_;
+	float p_term = e * p_gain;
+	float i_term = error_acc * i_gain;
+	int delta = pv - prev_input;
+	float d_term = delta * d_gain;
 
-	prev_input_ = pv;
+	prev_input = pv;
 
 	return p_term + i_term + d_term;
 }
--- v2/src/shared/PID.hh
+++ v2/src/shared/PID.hh
@@ -15,21 +15,21 @@
 /// * The output range is limited to 0-255.
 class PID {
 private:
-    float p_gain_; // proportional gain
-    float i_gain_; // integral gain
-    float d_gain_; // derivative gain
+    float p_gain; // proportional gain
+    float i_gain; // integral gain
+    float d_gain; // derivative gain
 
-    int prev_input_; // previous input for calculating delta
-    int error_acc_;  // accumulated error, for calculating integral
+    int prev_input; // previous input for calculating delta
+    int error_acc;  // accumulated error, for calculating integral
 
-    int sp_; // set point
+    int sp; // set point
 public:
-    void setPGain(const float p_gain) { p_gain_ = p_gain; }
-    void setIGain(const float i_gain) { i_gain_ = i_gain; }
-    void setDGain(const float d_gain) { d_gain_ = d_gain; }
+    void setPGain(const float p_gain_in) { p_gain = p_gain_in; }
+    void setIGain(const float i_gain_in) { i_gain = i_gain_in; }
+    void setDGain(const float d_gain_in) { d_gain = d_gain_in; }
 
-    void setTarget(const int target) { sp_ = target; }
-    const int getTarget() const { return sp_; }
+    void setTarget(const int target) { sp = target; }
+    const int getTarget() const { return sp; }
 
     void reset();
     /// PV is the process value; that is, the measured value
