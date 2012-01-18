1b8a07798b4973d36e837c04aa4f3031a8608a86
Alison Leonard <alison.nj.leonard@gmail.com>
2012-01-18 12:37:50 -0500

    dist updated

--- dist/MightyBoard/load_firmware.sh
+++ dist/MightyBoard/load_firmware.sh
@@ -1,19 +1,14 @@
 #!/bin/bash
 
-    echo "Press ENTER to upload 8U2 Bootloader"
+    echo "Press ENTER to upload 8U2 Firmware"
     read
     # Upload bootloader via isp
     avrdude -p at90usb82 -F -P usb -c avrispmkii -U flash:w:Makerbot-usbserial.hex -U lfuse:w:0xFF:m -U hfuse:w:0xD9:m -U efuse:w:0xF4:m -U lock:w:0x0F:m
 
-    echo "Press ENTER to upload 1280 Bootloader"
+    echo "Press ENTER to upload 1280 Firmware"
     read
     # Upload bootloader via isp
-    avrdude -p m1280 -F -P usb -c avrispmkii -U flash:w:ATmegaBOOT_168_atmega1280.hex -U lfuse:w:0xFF:m -U hfuse:w:0xDA:m -U efuse:w:0xF4:m -U lock:w:0x0F:m
-
-    echo "Press ENTER to upload firmware"
-    read
-    #Upload firmware
-    scons platform=mb40 port=/dev/ttyACM0 upload
+    avrdude -p m1280 -F -P usb -c avrispmkii -U flash:w:MightyBoard.hex -U lfuse:w:0xFF:m -U hfuse:w:0xDA:m -U efuse:w:0xF4:m -U lock:w:0x0F:m
 
 
 #!/bin/bash
