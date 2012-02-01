634c43e8e6ed0e075d257fac77e2b35e4bdb8565
Alison Leonard <alison.nj.leonard@gmail.com>
2012-02-01 15:07:02 -0500

    new load_firmware script including motor test

--- dist/MightyBoard/load_firmware.sh
+++ dist/MightyBoard/load_firmware.sh
@@ -1,36 +1,39 @@
 #!/bin/bash
 
-    echo "Press ENTER to upload 8U2 Firmware"
-    read
+FAIL8U2="8U2 Bootloader PASS"
+FAIL1280="1280 Bootloader PASS"
+FAILUSB="USB Program PASS"
+
     # Upload bootloader via isp
     avrdude -p at90usb82 -F -P usb -c usbtiny -U flash:w:Makerbot-usbserial.hex -U lfuse:w:0xFF:m -U hfuse:w:0xD9:m -U efuse:w:0xF4:m -U lock:w:0x0F:m
 
     if [ $? -ne 0 ]
     then
-     echo "8U2 Program FAIL"
+     FAIL8U2="8U2 Bootloader FAIL"
     fi
 
-    echo "Press ENTER to upload 1280 Firmware"
-    read
     # Upload bootloader via isp
-    avrdude -p m1280 -F -P usb -c avrispmkii -U flash:w:ATmegaBOOT_168_atmega1280.hex -U lfuse:w:0xFF:m -U hfuse:w:0xDA:m -U efuse:w:0xF4:m -U lock:w:0x0F:m
+    avrdude -p m1280 -F -P usb -c avrispmkii -U flash:w:motor_test.hex -U lfuse:w:0xFF:m -U hfuse:w:0xDA:m -U efuse:w:0xF4:m -U lock:w:0x0F:m
 
     if [ $? -ne 0 ]
      then
-      echo "1280 ISP Program FAIL"
+      FAIL1280="1280 Bootloader FAIL"
     fi
   
- echo "Press ENTER to test USB "
-    read
-    # Upload bootloader via isp
+   sleep 10   
+ 
+   # Upload bootloader via isp
    avrdude -p m1280 -P usb -c avrispmkii
    avrdude -F -V -p m1280 -P /dev/ttyACM0 -c stk500v1 -b 57600 -U flash:w:MightyBoard.hex
 
    if [ $? -ne 0 ]
     then
-       echo "USB Progam FAIL"
+       FAILUSB="USB Program FAIL"
     fi
 
+	echo $FAIL8U2
+	echo $FAIL1280
+	echo $FAILUSB
 
 #!/bin/bash
 
