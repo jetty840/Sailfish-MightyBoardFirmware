bb6588db5b8ab60220718902f08666598faefea5
Alison Leonard <alison.nj.leonard@gmail.com>
2012-04-11 12:04:21 -0400

    heater failure count accumulator fixed.  pivotal id 27851611

--- firmware/src/MightyBoard/shared/Heater.cc
+++ firmware/src/MightyBoard/shared/Heater.cc
@@ -219,6 +219,7 @@ void Heater::manage_temperature() {
 		}
 
 		current_temperature = sensor.getTemperature();
+		uint8_t old_value_count = value_fail_count;
 		// check that the the heater isn't reading above the maximum allowable temp
 		if (current_temperature > HEATER_CUTOFF_TEMPERATURE) {
 			value_fail_count++;
@@ -255,6 +256,10 @@ void Heater::manage_temperature() {
 					return;
 				}
 		}
+		// if no bad heat reads have occured, clear the fail count
+		// we don't want this to add up continually forever
+		if(value_fail_count == old_value_count)
+			value_fail_count = 0;
 	}
 	if (fail_state) {
 		return;
