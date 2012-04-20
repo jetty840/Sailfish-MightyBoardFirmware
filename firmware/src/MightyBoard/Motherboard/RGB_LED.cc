40f187cc177d4f8f2fd5f62da1e9db15252a026f
Alison Leonard <alison.nj.leonard@gmail.com>
2012-04-20 09:39:33 -0400

    alternate led driver test

--- firmware/src/MightyBoard/Motherboard/RGB_LED.cc
+++ firmware/src/MightyBoard/Motherboard/RGB_LED.cc
@@ -36,103 +36,20 @@ void init(){
 	 
 	 TWI_init();
 	 
+	 // make sure out drive is correct
+	 // verify pin out - RGB, i2c pins
+	 
+	 // set operation modes
+	 uint8_t data[2] = {LED_REG_MODE2, LED_OUT_INVERTED | LED_OUT_DRIVE};
+	 
 	 setDefaultColor();
  }
-    
- // channel : 1,2 select PWM channels, 3 is a pure on / off channel
- // level : duty cycle (brightness) for channels 1,2,  
- //			for Channel 3, level is on if not zero
- // LEDs:  {bits: XXBBGGRR : BLUE: 0b110000, Red:0b1100, Green:0b11} 
- //  		ones indicate on, zeros indicate off 
- void setBrightness(uint8_t Channel, uint8_t level, uint8_t LEDs)
- {
- 	uint8_t data[4] = {LED_REG_SELECT, 0, 0 , level};
-     uint8_t data1[2] = {LED_REG_SELECT, 0};
-     uint8_t data2[2] = {0, level};
+ 
 
-	// set pwm for select channel
- 	if (Channel == LED_CHANNEL1){
- 		data2[0] = LED_REG_PWM0;
- 		// clear past select data and apply PWM0
- 		data1[1] = (LEDSelect & ~LEDs) | (LED_BLINK_PWM0 & LEDs);
- 	}
- 	else if (Channel == LED_CHANNEL2){
- 		data2[0] = LED_REG_PWM1;
- 		// clear past select data and apply PWM1
- 		data1[1] = (LEDSelect & ~LEDs) | (LED_BLINK_PWM1 & LEDs);
- 	}
- 	else {
- 		toggleLEDNoPWM((level != 0), LEDs);
- 		return;
- 	}
- 	
- 	uint8_t error = TWI_write_data(LEDAddress, data1, 2);
-     _delay_us(1);
-    error = TWI_write_data(LEDAddress, data2, 2);
-     _delay_us(1);
- 	
-     LEDSelect = data1[1];
- 		
- }
-    
-  // channel : 1,2 select PWM channels, channel 3 does nothing
- // level : blink rate for channels 1,2,  channel 3 ignores this
- // LEDs:  {bits: XXBBGGRR : BLUE: 0b110000, Green:0b1100, RED:0b11} 
- //  		ones indicate on, zeros indicate off 
- void setBlinkRate(uint8_t Channel, uint8_t rate, uint8_t LEDs)
- {
-	 uint8_t data[4] = {0 , rate, LED_REG_SELECT, 0};
-     uint8_t data1[2] = {LED_REG_SELECT, 0};
-     uint8_t data2[2] = {0 , rate};
- 	
- 	// set pwm for select channel
- 	if (Channel == LED_CHANNEL1){
- 		data2[0] = LED_REG_PSC0;
- 		// clear past select data and apply PWM0
- 		data1[1] = (LEDSelect & ~LEDs) | (LED_BLINK_PWM0 & LEDs);
- 	}
- 	else if (Channel == LED_CHANNEL2){
- 		data2[0] = LED_REG_PSC1;
- 		// clear past select data and apply PWM1
- 		data1[1] = (LEDSelect & ~LEDs) | (LED_BLINK_PWM1 & LEDs);
- 	}
- 	else
- 		return;
- 	
-     uint8_t error = TWI_write_data(LEDAddress, data1, 2);
-     _delay_us(1);
-     error = TWI_write_data(LEDAddress, data2, 2);
-     _delay_us(1);
-     
- 	LEDSelect = data1[1];	
- }
- 
- // channel 3 sets LEDs on or off 
- // LEDs:  {bits: XXBBGGRR : BLUE: 0b110000, Green:0b1100, RED:0b11} 
- //  		ones indicate on, zeros indicate off 
- void toggleLEDNoPWM(bool enable, uint8_t LEDs)
- {
- 	uint8_t data[2] = {LED_REG_SELECT, 0};
- 	
- 	if(enable)
- 	// clear past select data and turn LEDs full on
- 		data[1] = (LEDSelect & ~LEDs) | (LED_ON & LEDs);
- 	else
- 	// clear past select data and turn LEDs full off
- 		data[1] = (LEDSelect & ~LEDs) | (LED_OFF & LEDs); 
- 		
- 	uint8_t error = TWI_write_data(LEDAddress, data, 2);
- 	
-     LEDSelect = data[1];
- }
- 
- void startupSequence(){
- }
      
  void clear(){
 	 
 	 // clear LEDs 
-    setBrightness(3, 0, LED_RED | LED_GREEN | LED_BLUE);
  }
  
 void errorSequence(){
@@ -140,8 +57,7 @@ void errorSequence(){
 	clear();
      
     // set blinking red lights
-    setBrightness(1, 200, LED_RED);
-    setBlinkRate(1, 130, LED_RED);    
+   
 }
 
 void setDefaultColor(){
