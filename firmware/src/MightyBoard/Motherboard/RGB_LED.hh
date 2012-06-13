54b6a25c16e054b77a06289ae1c3082c7ba894f9
aleonard <alison.nj.leonard@gmail.com>
2012-06-13 16:42:09 -0400

    replicator one only

--- firmware/src/MightyBoard/Motherboard/RGB_LED.hh
+++ firmware/src/MightyBoard/Motherboard/RGB_LED.hh
@@ -19,37 +19,23 @@
  #define RGB_LED_HH
  
  // LED control registers
- #define LED_REG_MODE1		0b00000000
- #define LED_REG_MODE2		0b00000001
- #define LED_REG_PWM_RED	0b00000010
- #define LED_REG_PWM_GREEN	0b00000011
- #define LED_REG_PWM_BLUE	0b00000100
- #define LED_REG_PWM3		0b00000101
- #define LED_REG_GRPPWM		0b00000110
- #define LED_REG_GRPFREQ	0b00000111
- #define LED_REG_LEDOUT		0b00001000
- #define LED_REG_SUBADR1	0b00001001
- #define LED_REG_SUBADR2	0b00001010
- #define LED_REG_SUBADR3	0b00001011
- #define LED_REG_ALLCALL  	0b00001100
- 
- /// LED DRIVER MODES
- #define LED_OUT_INVERTED	0b00010000
- #define LED_OUT_DRIVE		0b00000100
- #define LED_GROUP_BLINK	0b00100000
- #define LED_ALL_CALL_ADDR	0b00000001
+ #define LED_REG_PSC0	0b00000001
+ #define LED_REG_PWM0	0b00000010
+ #define LED_REG_PSC1	0b00000011
+ #define LED_REG_PWM1	0b00000100
+ #define LED_REG_SELECT  0b00000101
  
  
  // LED output types
- #define LED_INDIVIDUAL		0b10101010
- #define LED_GROUP		 	0b11111111
- #define LED_ON 			0b01010101
- #define LED_OFF			0b00000000
+ #define LED_BLINK_PWM0 0b10101010
+ #define LED_BLINK_PWM1 0b11111111
+ #define LED_ON 	0b01010101
+ #define LED_OFF	0b00000000
  
  // RBG IDs
- #define LED_GREEN 			0b00001100
- #define LED_RED 			0b00000011
- #define LED_BLUE 			0b00110000
+ #define LED_GREEN 0b00001100
+ #define LED_RED 0b00000011
+ #define LED_BLUE 0b00110000
  
  // Channel IDs
  #define LED_CHANNEL1	0
@@ -75,7 +61,7 @@ enum LEDColors{
  void init(); 
  void startupSequence();
  void errorSequence();
- void setColor(uint8_t red, uint8_t green, uint8_t blue, bool clearOld=true);
+ void setColor(uint8_t red, uint8_t green, uint8_t blue, bool clearOld);
  void setLEDBlink(uint8_t rate);
  void setDefaultColor();
  void setCustomColor(uint8_t red, uint8_t green, uint8_t blue);
