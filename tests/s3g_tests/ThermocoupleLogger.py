a26a5f01cdddedb1059435f6ab5eb4cc7395a69d
aleonard <alison.nj.leonard@gmail.com>
2012-08-15 11:45:18 -0400

    update thermocouple logger in tests

--- tests/s3g_tests/ThermocoupleLogger.py
+++ tests/s3g_tests/ThermocoupleLogger.py
@@ -20,21 +20,27 @@ import time
 
 s3g_port = s3g.s3g()
 
-def GetThermocoupleReads():
+def GetHeaterReads():
   
   log_file = csv.writer(open(options.filename, 'wb'), delimiter=' ')
   start_time = time.time()
 
   while 1:
+    temps = [time.time()-start_time]
     try:
-      raw_temp = s3g_port.get_toolhead_temperature(int(options.toolhead))
-      print "channel %d %d" % (int(options.toolhead), raw_temp)
+      if options.platform is not None:
+        platform_temp = s3g_port.get_platform_temperature(0)
+        temps.append(platform_temp)
+        print "platform %d" % (platform_temp)
+      if options.toolhead is not None:
+        raw_temp = s3g_port.get_toolhead_temperature(int(options.toolhead))
+        temps.append(raw_temp)
+        print "channel %d %d" % (int(options.toolhead), raw_temp)
       if options.toolhead_two is not None:
         raw_temp_0 =s3g_port.get_toolhead_temperature(int(options.toolhead_two)) 
-        log_file.writerow([time.time()-start_time, raw_temp, raw_temp_0])
+        temps.append(raw_temp_0)
         print "channel %d %d" % (int(options.toolhead_two), raw_temp_0)
-      else:
-        log_file.writerow([time.time()-start_time, raw_temp])
+      log_file.writerow(temps)
       time.sleep(0.495)
       
     except (KeyboardInterrupt) :
@@ -55,14 +61,16 @@ if __name__ == '__main__':
   parser = optparse.OptionParser()
   parser.add_option("-p", "--port", dest="serialPort", default="/dev/ttyACM0")
   parser.add_option("-f", "--file", dest="filename", default="temp_data.csv")
-  parser.add_option("-t", "--tool", dest="toolhead", default=0)
+  parser.add_option("-t", "--tool", dest="toolhead", default=None)
   parser.add_option("-w", "--tool_two", dest="toolhead_two", default=None)
+  parser.add_option("-m", "--platform", dest="platform", default=None)
   (options, args) = parser.parse_args()
 
   del sys.argv[1:]
 
   setUp();
-
-  GetThermocoupleReads()
+ 
+  GetHeaterReads()
+  
   tearDown()
     
