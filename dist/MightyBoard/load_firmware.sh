0f10faf0ea6c8b761d8d30567ba59c200216260d
aleonard <alison.nj.leonard@gmail.com>
2012-08-22 12:45:12 -0400

    update default 8U2 firmware

--- dist/MightyBoard/load_firmware.sh
+++ dist/MightyBoard/load_firmware.sh
@@ -31,7 +31,7 @@ FAILUSB="USB Program PASS"
     read
 
     # Upload bootloader via isp
-    avrdude -p at90usb82 -F -P usb -c avrispmkii -U flash:w:Makerbot-usbserial.hex -U lfuse:w:0xFF:m -U hfuse:w:0xD9:m -U efuse:w:0xF4:m -U lock:w:0x0F:m
+    avrdude -p at90usb82 -F -P usb -c avrispmkii -U flash:w:Makerbot-usbserial-rep2.hex -U lfuse:w:0xFF:m -U hfuse:w:0xD9:m -U efuse:w:0xF4:m -U lock:w:0x0F:m
 
     if [ $? -ne 0 ]
     then
---
+++ docs/known_issues.md
@@ -0,0 +1,3 @@
+
+
+Bot cannot read file names longer that 32 characters.  There is no warning to the user about this, the filenames just do not show up.
