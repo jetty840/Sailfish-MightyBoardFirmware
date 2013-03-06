0fe025a5a9e83808efd4ce4a815c45e866e369d8
Dan Newman <dan.newman@mtbaldy.us>
2013-03-05 16:29:08 -0800

    Tweaks to save code space

--- firmware/src/MightyBoard/shared/InterfaceBoard.hh
+++ firmware/src/MightyBoard/shared/InterfaceBoard.hh
@@ -54,8 +54,6 @@ private:
         Screen* screenStack[SCREEN_STACK_DEPTH];
         int8_t screenIndex;             ///< Stack index of the current screen.
 
-        Pin LEDs[2];                    ///< Pins connected to the LEDs
-
         /// TODO: Delete this.
         bool building;                  ///< True if the bot is building
         
@@ -79,8 +77,6 @@ public:
         /// \param[in] Screen to display while building
         InterfaceBoard(ButtonArray& buttons_in,
                        LiquidCrystalSerial& lcd_in,
-                       const Pin& gled,
-                       const Pin& rled,
                        Screen* mainScreen_in,
                        Screen* buildScreen_in,
                        MessageScreen* messageScreen_in,
