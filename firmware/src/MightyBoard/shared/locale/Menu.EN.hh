44103d39c30c46cb19e8940b0d0d1ed01698da88
Felipe Corrêa da Silva Sanches <fsanches@metamaquina.com.br>
2013-12-13 22:40:01 -0200

    fixing the french version of the firmware

--- firmware/src/MightyBoard/shared/locale/Menu.EN.hh
+++ firmware/src/MightyBoard/shared/locale/Menu.EN.hh
@@ -1,9 +1,9 @@
-#ifndef FRENCH
-#	error no french local defined!
-#endif
+
+
+
 
 #ifdef MODEL_REPLICATOR
-const static PROGMEM prog_uchar SPLASH1_MSG[] = "   La Replicator    ";
+const static PROGMEM prog_uchar SPLASH1_MSG[] = "Sailfish Replicator1";
 #if !defined(HEATERS_ON_STEROIDS)
 const static PROGMEM prog_uchar SPLASH2_MSG[] = "--------------------";
 #else
@@ -33,66 +33,68 @@ const static PROGMEM prog_uchar SPLASH2A_MSG[] = "    SUCCESS!        ";
 const static PROGMEM prog_uchar SPLASH3A_MSG[] = "connected correctly ";
 const static PROGMEM prog_uchar SPLASH4A_MSG[] = "Heaters are not     ";
 
-const static PROGMEM prog_uchar GO_MSG[] =         "Prechauffer";
-const static PROGMEM prog_uchar STOP_MSG[] =       "Refroidir";
-const static PROGMEM prog_uchar PLATFORM_MSG[] =   "Platforme";
-const static PROGMEM prog_uchar TOOL_MSG[] =       "Extrudeur";
+const static PROGMEM prog_uchar GO_MSG[] =         "Start Preheating";
+const static PROGMEM prog_uchar STOP_MSG[] =       "Turn Heaters Off";
+const static PROGMEM prog_uchar PLATFORM_MSG[] =   "Platform";
+const static PROGMEM prog_uchar TOOL_MSG[] =       "Extruder";
 const static PROGMEM prog_uchar NOZZLE_ERROR_MSG[] = "Toolhead Offset Sys " "must be set to NEW  " "to use this utility.";
 const static PROGMEM prog_uchar START_TEST_MSG[]=  "I'm going to print  " "a series of lines so" "we can find my      " "nozzle alignment.   ";
 const static PROGMEM prog_uchar EXPLAIN1_MSG[] =   "Look for the best   " "matched line in each" "axis set.  Lines are" "numbered 1-13 and...";
 const static PROGMEM prog_uchar EXPLAIN2_MSG[] =   "line one is extra   " "long. The Y axis set" "is left on the plate" "and X axis is right.";
 const static PROGMEM prog_uchar END_MSG  [] =      "Great!  I've saved  " "these settings and  " "I'll use them to    " "make nice prints!   ";
 
-const static PROGMEM prog_uchar SELECT_MSG[] =     "Selectionnez les    meilleures lignes.";
-const static PROGMEM prog_uchar DONE_MSG[]   =     "Fini!";
-const static PROGMEM prog_uchar NO_MSG[]   =       "Non";
-const static PROGMEM prog_uchar YES_MSG[]  =       "Oui";
+const static PROGMEM prog_uchar SELECT_MSG[] =     "Select best lines.";
+const static PROGMEM prog_uchar DONE_MSG[]   =     "Done";
+const static PROGMEM prog_uchar NO_MSG[]   =       "NO ";
+const static PROGMEM prog_uchar YES_MSG[]  =       "YES";
 
-const static PROGMEM prog_uchar XAXIS_MSG[] =      "Axe X";
-const static PROGMEM prog_uchar YAXIS_MSG[] =      "Axe Y";
-const static PROGMEM prog_uchar HEATER_ERROR_MSG[]=  "Les extrudeurs ne   " "chauffent pas.      " "Verifiez les        " "connexions!";
-const static PROGMEM prog_uchar STOP_EXIT_MSG[]   = "Quand le filament   " "sort de la buse,    " "appuyez sur M pour  " "quitter.";
+const static PROGMEM prog_uchar XAXIS_MSG[] =      "X Axis Line";
+const static PROGMEM prog_uchar YAXIS_MSG[] =      "Y Axis Line";
+
+const static PROGMEM prog_uchar HEATER_ERROR_MSG[]=  "Extruders are not   " "heating.  Check the " "wiring.";
+
+const static PROGMEM prog_uchar STOP_EXIT_MSG[]   = "Begin loading or    " "unloading. Press 'M'" "to exit.";
 const static PROGMEM prog_uchar TIMEOUT_MSG[]  	  = "5 minute timeout has" "elapsed.  Press 'M' " "to exit.";
 
-const static PROGMEM prog_uchar LOAD_RIGHT_MSG[]    = "Mettre a droite"; 
-const static PROGMEM prog_uchar LOAD_LEFT_MSG[]     = "Mettre a gauche"; 
-const static PROGMEM prog_uchar LOAD_SINGLE_MSG[]   = "Mettre le plastique"; 
-const static PROGMEM prog_uchar UNLOAD_SINGLE_MSG[] = "Retirer le plastique"; 
-const static PROGMEM prog_uchar UNLOAD_RIGHT_MSG[]  = "Retirer a gauche"; 
-const static PROGMEM prog_uchar UNLOAD_LEFT_MSG[]   = "Retirer a droite"; 
+const static PROGMEM prog_uchar LOAD_RIGHT_MSG[]    = "Load right";
+const static PROGMEM prog_uchar LOAD_LEFT_MSG[]     = "Load left";
+const static PROGMEM prog_uchar LOAD_SINGLE_MSG[]   = "Load";
+const static PROGMEM prog_uchar UNLOAD_SINGLE_MSG[] = "Unload";
+const static PROGMEM prog_uchar UNLOAD_RIGHT_MSG[]  = "Unload right";
+const static PROGMEM prog_uchar UNLOAD_LEFT_MSG[]   = "Unload left";
 
-const static PROGMEM prog_uchar JOG1_MSG[]  = "    Mode manuel     ";
+const static PROGMEM prog_uchar JOG1_MSG[]  = "     Jog mode       ";
 const static PROGMEM prog_uchar JOG2X_MSG[] = "        X+          ";
-const static PROGMEM prog_uchar JOG3X_MSG[] = "      Retour   Y->  ";
+const static PROGMEM prog_uchar JOG3X_MSG[] = "      (Back)   Y->  ";
 const static PROGMEM prog_uchar JOG4X_MSG[] = "        X-          ";
-const static PROGMEM prog_uchar JOG3Y_MSG[] = "  <-X Retour  Z->   ";
-const static PROGMEM prog_uchar JOG3Z_MSG[] = "  <-Y Retour        ";
+const static PROGMEM prog_uchar JOG3Y_MSG[] = "  <-X (Back)  Z->   ";
+const static PROGMEM prog_uchar JOG3Z_MSG[] = "  <-Y (Back)        ";
 
 
-const static PROGMEM prog_uchar DISTANCESHORT_MSG[] = "COURT";
+const static PROGMEM prog_uchar DISTANCESHORT_MSG[] = "SHORT";
 const static PROGMEM prog_uchar DISTANCELONG_MSG[] =  "LONG";
 
-const static PROGMEM prog_uchar HEATING_MSG[] =        "Chauffe:";
-const static PROGMEM prog_uchar HEATING_SPACES_MSG[] = "Chauffe:            ";
+const static PROGMEM prog_uchar HEATING_MSG[] =        "Heating:";
+const static PROGMEM prog_uchar HEATING_SPACES_MSG[] = "Heating:            ";
 
 const static PROGMEM prog_uchar BUILD_PERCENT_MSG[] =    " --%";
-const static PROGMEM prog_uchar EXTRUDER1_TEMP_MSG[] =   "Tete Droite ---/---C";
-const static PROGMEM prog_uchar EXTRUDER2_TEMP_MSG[] =   "Tete Gauche ---/---C";
-const static PROGMEM prog_uchar PLATFORM_TEMP_MSG[]  =   "Plateforme  ---/---C";
-const static PROGMEM prog_uchar EXTRUDER_TEMP_MSG[]  =   "Extrudeur:  ---/---C";
+const static PROGMEM prog_uchar EXTRUDER1_TEMP_MSG[] =   "R Extruder: ---/---C";
+const static PROGMEM prog_uchar EXTRUDER2_TEMP_MSG[] =   "L Extruder: ---/---C";
+const static PROGMEM prog_uchar PLATFORM_TEMP_MSG[]  =   "Platform:   ---/---C";
+const static PROGMEM prog_uchar EXTRUDER_TEMP_MSG[]  =   "Extruder:   ---/---C";
 
 
 const static PROGMEM prog_uchar EXTRUDER_SPACES_MSG[]  = "Extruder           ";
-const static PROGMEM prog_uchar RIGHT_SPACES_MSG[]     = "Tete de droite     ";
-const static PROGMEM prog_uchar LEFT_SPACES_MSG[]      = "Tete de gauche     ";
-const static PROGMEM prog_uchar PLATFORM_SPACES_MSG[]  = "Platforme          ";
-const static PROGMEM prog_uchar RESET1_MSG[]           = "Retour parametres";
-const static PROGMEM prog_uchar RESET2_MSG[]           = "d'usine?";
-const static PROGMEM prog_uchar CANCEL_MSG[] = "Annuler impression?";
+const static PROGMEM prog_uchar RIGHT_SPACES_MSG[]     = "Right Tool         ";
+const static PROGMEM prog_uchar LEFT_SPACES_MSG[]      = "Left Tool          ";
+const static PROGMEM prog_uchar PLATFORM_SPACES_MSG[]  = "Platform           ";
+const static PROGMEM prog_uchar RESET1_MSG[]           = "Restore factory";
+const static PROGMEM prog_uchar RESET2_MSG[]           = "settings?";
+const static PROGMEM prog_uchar CANCEL_MSG[] = "Cancel this print?";
 const static PROGMEM prog_uchar CANCEL_FIL_MSG[] = "Cancel load/unload?";
 
-const static PROGMEM prog_uchar PAUSE_MSG[]      = "Pause    "; // must be same length as the next msg
-const static PROGMEM prog_uchar UNPAUSE_MSG[]    = "Reprendre"; // must be same length as the previous msg
+const static PROGMEM prog_uchar PAUSE_MSG[]      = "Pause  "; // must be same length as the next msg
+const static PROGMEM prog_uchar UNPAUSE_MSG[]    = "Unpause"; // must be same length as the prior msg
 const static PROGMEM prog_uchar COLD_PAUSE_MSG[] = "Cold Pause";
 
 const static PROGMEM prog_uchar BUILDING_MSG[]    = "I'm already building";
@@ -107,31 +109,31 @@ const static PROGMEM prog_uchar CARDSIZE_MSG[]    = "Unable to read SD   " "card
 const static PROGMEM prog_uchar EXTRUDER_TIMEOUT_MSG[] = "I timed out while   attempting to heat  my extruder."; 
 const static PROGMEM prog_uchar PLATFORM_TIMEOUT_MSG[] = "I timed out while   attempting to heat  my platform.";
 
-const static PROGMEM prog_uchar BUILD_MSG[] =            "Imprimer depuis SD";
-const static PROGMEM prog_uchar PREHEAT_MSG[] =          "Prechauffage";
-const static PROGMEM prog_uchar UTILITIES_MSG[] =        "Utilitaires";
-const static PROGMEM prog_uchar MONITOR_MSG[] =          "Visu Temp   ";
-const static PROGMEM prog_uchar JOG_MSG[]   =            "Mode Manuel";
-const static PROGMEM prog_uchar CALIBRATION_MSG[] =      "Calibration des axes";
-const static PROGMEM prog_uchar HOME_AXES_MSG[] =        "Axes a l'origine";
-const static PROGMEM prog_uchar FILAMENT_OPTIONS_MSG[] = "Options du filament";
-const static PROGMEM prog_uchar VERSION_MSG[] = 		     "Numero de Version";
-const static PROGMEM prog_uchar DSTEPS_MSG[] =           "Debloquer Moteurs";
-const static PROGMEM prog_uchar ESTEPS_MSG[] =           "Bloquer Moteurs  ";
-const static PROGMEM prog_uchar PLATE_LEVEL_MSG[] =      "Relever Plateforme";
-const static PROGMEM prog_uchar LED_BLINK_MSG[] =        "Clignotement LED ";
-const static PROGMEM prog_uchar LED_STOP_MSG[] =         "Stop Clignotement";
-const static PROGMEM prog_uchar PREHEAT_SET_MSG[] =      "Param. prechauffage ";
-const static PROGMEM prog_uchar SETTINGS_MSG[] =         "Parametres Generaux";
-const static PROGMEM prog_uchar RESET_MSG[] =            "RAZ usine";
-const static PROGMEM prog_uchar NOZZLES_MSG[] =          "Calibration Buses";
+const static PROGMEM prog_uchar BUILD_MSG[] =            "Print from SD";
+const static PROGMEM prog_uchar PREHEAT_MSG[] =          "Preheat";
+const static PROGMEM prog_uchar UTILITIES_MSG[] =        "Utilities";
+const static PROGMEM prog_uchar MONITOR_MSG[] =          "Monitor Mode";
+const static PROGMEM prog_uchar JOG_MSG[]   =            "Jog Mode";
+const static PROGMEM prog_uchar CALIBRATION_MSG[] =      "Calibrate Axes";
+const static PROGMEM prog_uchar HOME_AXES_MSG[] =        "Home Axes";
+const static PROGMEM prog_uchar FILAMENT_OPTIONS_MSG[] = "Filament Loading";
+const static PROGMEM prog_uchar VERSION_MSG[] = 	       "Version Information";
+const static PROGMEM prog_uchar DSTEPS_MSG[] =           "Disable Steppers";
+const static PROGMEM prog_uchar ESTEPS_MSG[] =           "Enable Steppers  ";
+const static PROGMEM prog_uchar PLATE_LEVEL_MSG[] =      "Level Build Plate";
+const static PROGMEM prog_uchar LED_BLINK_MSG[] =        "Blink LEDs       ";
+const static PROGMEM prog_uchar LED_STOP_MSG[] =         "Stop Blinking!";
+const static PROGMEM prog_uchar PREHEAT_SET_MSG[] =      "Preheat Settings";
+const static PROGMEM prog_uchar SETTINGS_MSG[] =         "General Settings";
+const static PROGMEM prog_uchar RESET_MSG[] =            "Restore Settings";
+const static PROGMEM prog_uchar NOZZLES_MSG[] =          "Calibrate Nozzles";
 const static PROGMEM prog_uchar BOT_STATS_MSG[] =        "Bot Statistics";
-const static PROGMEM prog_uchar TOOL_COUNT_MSG[] =       "Nb Tetes:";
-const static PROGMEM prog_uchar SOUND_MSG[] =            "Son";
-const static PROGMEM prog_uchar LED_MSG[] =              "Eclairage";
-const static PROGMEM prog_uchar LED_HEAT_MSG[] =         "Coul Chauf";
-const static PROGMEM prog_uchar EXIT_MSG[] =             "Sortir du Menu";
-const static PROGMEM prog_uchar ACCELERATE_MSG[] = 	     "Accelerer";
+const static PROGMEM prog_uchar TOOL_COUNT_MSG[] =       "Extruders";
+const static PROGMEM prog_uchar SOUND_MSG[] =            "Sound";
+const static PROGMEM prog_uchar LED_MSG[] =              "LED Colour";
+const static PROGMEM prog_uchar LED_HEAT_MSG[] =         "Heat LEDs";
+const static PROGMEM prog_uchar EXIT_MSG[] =             "Exit menu";
+const static PROGMEM prog_uchar ACCELERATE_MSG[] = 	     "Accelerate";
 const static PROGMEM prog_uchar HBP_MSG[]                 = "HBP installed";
 const static PROGMEM prog_uchar OVERRIDE_GCODE_TEMP_MSG[] = "Override GcTemp";
 const static PROGMEM prog_uchar PAUSE_HEAT_MSG[]	        = "Pause with Heat";
@@ -155,38 +157,31 @@ const static PROGMEM prog_uchar PRINT_ANOTHER_MSG[]       = "Print Another Copy"
 const static PROGMEM prog_uchar CANNOT_PRINT_ANOTHER_MSG[]= "(Cannot Print Copy)";
 
 
-const static PROGMEM prog_uchar RED_COLOR_MSG[]    = "ROUGE ";
+const static PROGMEM prog_uchar RED_COLOR_MSG[]    = "RED   ";
 const static PROGMEM prog_uchar ORANGE_COLOR_MSG[] = "ORANGE";
-const static PROGMEM prog_uchar PINK_COLOR_MSG[]   = "ROSE  ";
-const static PROGMEM prog_uchar GREEN_COLOR_MSG[]  = "VERT  ";
-const static PROGMEM prog_uchar BLUE_COLOR_MSG[]   = "BLEU  ";
-const static PROGMEM prog_uchar PURPLE_COLOR_MSG[] = "VIOLET";
-const static PROGMEM prog_uchar WHITE_COLOR_MSG[]  = "BLANC ";
-const static PROGMEM prog_uchar CUSTOM_COLOR_MSG[] = "AUTRE ";
+const static PROGMEM prog_uchar PINK_COLOR_MSG[]   = "PINK  ";
+const static PROGMEM prog_uchar GREEN_COLOR_MSG[]  = "GREEN ";
+const static PROGMEM prog_uchar BLUE_COLOR_MSG[]   = "BLUE  ";
+const static PROGMEM prog_uchar PURPLE_COLOR_MSG[] = "PURPLE";
+const static PROGMEM prog_uchar WHITE_COLOR_MSG[]  = "WHITE ";
+const static PROGMEM prog_uchar CUSTOM_COLOR_MSG[] = "CUSTOM";
 const static PROGMEM prog_uchar OFF_COLOR_MSG[]    = "OFF   ";
 
-const static PROGMEM prog_uchar ERROR_MSG[] =   "erreur";
+const static PROGMEM prog_uchar ERROR_MSG[] =   "error!";
 const static PROGMEM prog_uchar NA_MSG[] =      "  NA    ";
-const static PROGMEM prog_uchar WAITING_MSG[] = "attente ";
+const static PROGMEM prog_uchar WAITING_MSG[] = "waiting ";
 
 const static PROGMEM prog_uchar EXTEMP_CHANGE_MSG[]               = "My temperature was  " "changed externally. " "Reselect filament   " "menu to try again.";
-const static PROGMEM prog_uchar HEATER_INACTIVITY_MSG[]           = "Exctincion des tetes" "due a l'inactivite";
+const static PROGMEM prog_uchar HEATER_INACTIVITY_MSG[]           = "Heaters shutdown due" "to inactivity";
 const static PROGMEM prog_uchar HEATER_TOOL_MSG[]                 = "Extruder Failure!   ";
 const static PROGMEM prog_uchar HEATER_TOOL0_MSG[]                = "Tool 0 Failure!     ";
 const static PROGMEM prog_uchar HEATER_TOOL1_MSG[]                = "Tool 1 Failure!     ";
 const static PROGMEM prog_uchar HEATER_PLATFORM_MSG[]             = "Platform Failure!   ";
 
-//The following messages are in english because the french translation is too large
-//and results in a firmware image that does not fit in the available flash memory space
-//Please try to save bytes maybe by using some abreviations
 const static PROGMEM prog_uchar HEATER_FAIL_SOFTWARE_CUTOFF_MSG[] = "Temp limit reached. " "Shutdown or restart.";
 const static PROGMEM prog_uchar HEATER_FAIL_NOT_HEATING_MSG[]     = "Not heating properly" "Check wiring.";
 const static PROGMEM prog_uchar HEATER_FAIL_DROPPING_TEMP_MSG[]   = "Temperature dropping" "Check wiring.";
 const static PROGMEM prog_uchar HEATER_FAIL_NOT_PLUGGED_IN_MSG[]  = "Temperature reads   " "are failing.        " "Check wiring.";
-//const static PROGMEM prog_uchar HEATER_FAIL_SOFTWARE_CUTOFF_MSG[] = "Surchauffe des tetes" "Temperature limite  " "Atteinte! Veuillez  " "Redemarrer";
-//const static PROGMEM prog_uchar HEATER_FAIL_NOT_HEATING_MSG[]     = "Surchauffe des tetes" "Elles ne chauffent  " "pas correctement !  " "Verif. connectiques ";
-//const static PROGMEM prog_uchar HEATER_FAIL_DROPPING_TEMP_MSG[]   = "Echec du chauffage !" "La temperature des  " "tetes chute !       " "Verif. connectiques ";
-//const static PROGMEM prog_uchar HEATER_FAIL_NOT_PLUGGED_IN_MSG[]  = "Erreur de chauffe ! " "Echec du releve de  " "temperature!        " "Verif. connectiques ";
 const static PROGMEM prog_uchar HEATER_FAIL_READ_MSG[]            = "Temperature reads   " "out of range.       " "Check wiring.";
 
 const static PROGMEM prog_uchar BUILD_TIME_MSG[]	= "Print Time:     h  m"; 
@@ -252,3 +247,5 @@ const static PROGMEM prog_uchar ERROR_BOT_TYPE[] =
 const static PROGMEM prog_uchar ERROR_BOT_TYPE[] =
     "I am a Replicator.  This build is for   another bot. See:   makerbot.com/help";
 #endif
+
+
