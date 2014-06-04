4de6460cb15525265367795352278d45b4159e31
Dan Newman <dan.newman@mtbaldy.us>
2014-06-03 23:04:05 -0700

    More build system cleanup

--- firmware/src/platforms.py
+++ firmware/src/platforms.py
@@ -5,29 +5,61 @@ platforms = {
           'programmer' : 'stk500v1',
           'model' : 'REPLICATOR',
           'board_platform' : 'mighty_one',
-          'squeeze' : [ 'Menu.cc', 'Interface.cc', 'InterfaceBoard.cc', 'LiquidCrystalSerial.cc',
-                        'DigiPots.cc', 'PSU.cc', 'Eeprom.cc', 'PSU.cc', 'EepromMap.cc', 'Piezo.cc',
+          'squeeze' : [ 'Menu.cc', 'Interface.cc', 'InterfaceBoard.cc',
+                        'LiquidCrystalSerial.cc', 'DigiPots.cc', 'PSU.cc',
+                        'Eeprom.cc', 'PSU.cc', 'EepromMap.cc', 'Piezo.cc',
                         'UtilityScripts.cc' ]
           },
+
+    'mighty_one-corexy' :
+        { 'mcu' : 'atmega1280',
+          'programmer' : 'stk500v1',
+          'model' : 'REPLICATOR',
+          'board_platform' : 'mighty_one',
+          'squeeze' : [ 'Menu.cc', 'Interface.cc', 'InterfaceBoard.cc',
+                        'LiquidCrystalSerial.cc', 'DigiPots.cc', 'PSU.cc',
+                        'Eeprom.cc', 'PSU.cc', 'EepromMap.cc', 'Piezo.cc',
+                        'UtilityScripts.cc' ],
+          'defines' : [ 'CORE_XY', 'HEATERS_ON_STEROIDS' ]
+        },
 
     'mighty_one-2560' :
         { 'mcu' : 'atmega2560',
           'programmer' : 'stk500v2',
           'model' : 'REPLICATOR',
           'board_platform' : 'mighty_one',
-          'defines' : ['BUILD_STATS', 'ALTERNATE_UART']
-          },
+          'defines' : [ 'BUILD_STATS', 'ALTERNATE_UART' ]
+        },
+
+    'mighty_one-2560-corexy' :
+        { 'mcu' : 'atmega2560',
+          'programmer' : 'stk500v2',
+          'model' : 'REPLICATOR',
+          'board_platform' : 'mighty_one',
+          'defines' : [ 'CORE_XY', 'BUILD_STATS', 'ALTERNATE_UART',
+                        'HEATERS_ON_STEROIDS' ]
+        },
+
+    'mighty_one-2560-max31855' :
+        { 'mcu' : 'atmega2560',
+          'programmer' : 'stk500v2',
+          'model' : 'REPLICATOR',
+          'board_platform' : 'mighty_one',
+          'defines' : [ 'BUILD_STATS', 'ALTERNATE_UART', 'MAX31855' ]
+        },
 
     'mighty_two' :
         { 'mcu' : 'atmega1280',
           'programmer' : 'stk500v1',
           'model' : 'REPLICATOR2',
           'board_platform' : 'mighty_two',
-          'defines' : ['SINGLE_EXTRUDER', 'BUILD_STATS'],
-          'squeeze' : [ 'Menu.cc', 'Interface.cc', 'InterfaceBoard.cc', 'LiquidCrystalSerial.cc',
-                        'DigiPots.cc', 'PSU.cc', 'Eeprom.cc', 'PSU.cc', 'EepromMap.cc', 'Piezo.cc',
-                        'UtilityScripts.cc', 'TemperatureTable.cc', 'Thermistor.cc', 'Thermocouple.cc',
-                        'Heater.cc', 'CoolingFan.cc', 'PID.cc',
+          'defines' : [ 'SINGLE_EXTRUDER', 'BUILD_STATS' ],
+          'squeeze' : [ 'Menu.cc', 'Interface.cc', 'InterfaceBoard.cc',
+                        'LiquidCrystalSerial.cc', 'DigiPots.cc', 'PSU.cc',
+                        'Eeprom.cc', 'PSU.cc', 'EepromMap.cc', 'Piezo.cc',
+                        'UtilityScripts.cc', 'TemperatureTable.cc',
+                        'Thermistor.cc', 'Thermocouple.cc', 'Heater.cc',
+                        'CoolingFan.cc', 'PID.cc',
   '[ os.path.basename(f) for f in glob.glob(\'../../src/MightyBoard/Motherboard/lib_sd/*.c\') ]',
   '[ os.path.basename(f) for f in glob.glob(\'../../src/MightyBoard/Motherboard/boards/mighty_two/*.cc\') ]' ]
         },
@@ -37,18 +69,20 @@ platforms = {
           'programmer' : 'stk500v2',
           'model' : 'REPLICATOR2',
           'board_platform' : 'mighty_two',
-          'defines' : ['SINGLE_EXTRUDER', 'BUILD_STATS', 'ALTERNATE_UART']
-          },
+          'defines' : [ 'SINGLE_EXTRUDER', 'BUILD_STATS', 'ALTERNATE_UART' ]
+        },
 
     'mighty_twox' :
         { 'mcu' : 'atmega1280',
           'programmer' : 'stk500v1',
           'model' : 'REPLICATOR2',
           'board_platform' : 'mighty_two',
-          'squeeze' : [ 'Menu.cc', 'Interface.cc', 'InterfaceBoard.cc', 'LiquidCrystalSerial.cc',
-                        'DigiPots.cc', 'PSU.cc', 'Eeprom.cc', 'PSU.cc', 'EepromMap.cc', 'Piezo.cc',
-                        'UtilityScripts.cc', 'TemperatureTable.cc', 'Thermistor.cc', 'Thermocouple.cc',
-                        'Heater.cc', 'CoolingFan.cc', 'PID.cc',
+          'squeeze' : [ 'Menu.cc', 'Interface.cc', 'InterfaceBoard.cc',
+                        'LiquidCrystalSerial.cc', 'DigiPots.cc', 'PSU.cc',
+                        'Eeprom.cc', 'PSU.cc', 'EepromMap.cc', 'Piezo.cc',
+                        'UtilityScripts.cc', 'TemperatureTable.cc',
+                        'Thermistor.cc', 'Thermocouple.cc', 'Heater.cc',
+                        'CoolingFan.cc', 'PID.cc',
   '[ os.path.basename(f) for f in glob.glob(\'../../src/MightyBoard/Motherboard/boards/mighty_two/*.cc\') ]' ]
         },
 
@@ -57,7 +91,7 @@ platforms = {
           'programmer' : 'stk500v2',
           'model' : 'REPLICATOR2',
           'board_platform' : 'mighty_two',
-          'defines' : ['BUILD_STATS', 'ALTERNATE_UART']
+          'defines' : [ 'BUILD_STATS', 'ALTERNATE_UART' ]
         },
 
     'ff_creator' :
@@ -65,9 +99,11 @@ platforms = {
           'programmer' : 'stk500v1',
           'model' : 'REPLICATOR',
           'board_platform' : 'mighty_one',
-          'squeeze' : [ 'Menu.cc', 'Interface.cc', 'InterfaceBoard.cc', 'LiquidCrystalSerial.cc',
-                        'DigiPots.cc', 'PSU.cc', 'Eeprom.cc', 'PSU.cc', 'EepromMap.cc', 'Piezo.cc',
-                        'UtilityScripts.cc' ]
+          'squeeze' : [ 'Menu.cc', 'Interface.cc', 'InterfaceBoard.cc',
+                        'LiquidCrystalSerial.cc', 'DigiPots.cc', 'PSU.cc',
+                        'Eeprom.cc', 'PSU.cc', 'EepromMap.cc', 'Piezo.cc',
+                        'UtilityScripts.cc' ],
+          'defines' :  [ 'FF_CREATOR', 'HEATERS_ON_STEROIDS' ]
         },
 
     'ff_creator-2560' :
@@ -75,7 +111,8 @@ platforms = {
           'programmer' : 'stk500v2',
           'model' : 'REPLICATOR',
           'board_platform' : 'mighty_one',
-          'defines' : ['BUILD_STATS', 'ALTERNATE_UART']
+          'defines' : [ 'FF_CREATOR', 'BUILD_STATS', 'ALTERNATE_UART',
+                        'HEATERS_ON_STEROIDS' ]
         },
 
     'ff_creatorx-2560' :
@@ -83,17 +120,25 @@ platforms = {
           'programmer' : 'stk500v2',
           'model' : 'REPLICATOR',
           'board_platform' : 'mighty_one',
-          'defines' : ['BUILD_STATS', 'ALTERNATE_UART']
-          },
+          'defines' : [ 'BUILD_STATS', 'ALTERNATE_UART', 'FF_CREATOR_X',
+                        'HEATERS_ON_STEROIDS' ]
+        },
 
     'wanhao_dup4' :
         { 'mcu' : 'atmega1280',
           'programmer' : 'stk500v1',
           'model' : 'REPLICATOR',
           'board_platform' : 'mighty_one',
-          'squeeze' : [ 'Menu.cc', 'Interface.cc', 'InterfaceBoard.cc', 'LiquidCrystalSerial.cc',
-                        'DigiPots.cc', 'PSU.cc', 'Eeprom.cc', 'PSU.cc', 'EepromMap.cc', 'Piezo.cc',
-                        'UtilityScripts.cc' ]
+          'squeeze' : [ 'Menu.cc', 'Interface.cc', 'InterfaceBoard.cc',
+                        'LiquidCrystalSerial.cc','DigiPots.cc', 'PSU.cc',
+                        'Eeprom.cc', 'PSU.cc', 'EepromMap.cc', 'Piezo.cc',
+                        'UtilityScripts.cc' ],
+          'defines' : [ 'WANHAO_DUP4', 'HEATERS_ON_STEROIDS', '-HAS_RGB_LED' ]
         }
 }
 
+if __name__ == '__main__':
+    list = ''
+    for key in platforms:
+        list += key + ' '
+    print list[:-1]
