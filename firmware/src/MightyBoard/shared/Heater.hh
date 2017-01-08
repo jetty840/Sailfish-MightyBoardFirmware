709ab231de3fd8a1866fb177a0fab78c27c7e171
Joachim <joachim.dihlmann@onpex.com>
2017-01-08 18:32:53 +0100

    Put Heater_Max_Temp to PROGMEM /2

--- firmware/src/MightyBoard/shared/Heater.hh
+++ firmware/src/MightyBoard/shared/Heater.hh
@@ -29,9 +29,9 @@
 //#define MAX_HBP_TEMP   130
 
 #ifdef HEATER_CUSTOM_LIMIT
-const static PROGMEM uint16_t Heater_Max_Temp[2] = HEATER_CUSTOM_LIMIT;
+const static uint16_t Heater_Max_Temp[2] PROGMEM = HEATER_CUSTOM_LIMIT;
 #else
-const static PROGMEM uint16_t Heater_Max_Temp[2] = {280,130};
+const static uint16_t Heater_Max_Temp[2] PROGMEM = {280,130};
 #endif
 
 #  define MAX_VALID_TEMP Heater_Max_Temp[0]
