7571e57f57d626f4ac8aed9e16637f3f560d0688
Joachim <joachim.dihlmann@onpex.com>
2019-02-20 15:30:52 +0100

    pid min 0

--- firmware/src/MightyBoard/shared/PID.cc
+++ firmware/src/MightyBoard/shared/PID.cc
@@ -27,7 +27,7 @@
 #include "PID.hh"
 
 #define ERR_ACC_MAX 256
-#define ERR_ACC_MIN -ERR_ACC_MAX
+#define ERR_ACC_MIN 0      //-ERR_ACC_MAX
 
 // scale the output term to account for our fixed-point bounds
 #define OUTPUT_SCALE 2
