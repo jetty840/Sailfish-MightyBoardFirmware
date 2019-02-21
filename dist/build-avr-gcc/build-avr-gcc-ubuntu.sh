8beeac2d71c178c92f5b02ae201da879fe3852ca
Joachim <joachim.dihlmann@onpex.com>
2019-02-21 09:44:42 +0100

    avr gcc

--- dist/build-avr-gcc/build-avr-gcc-ubuntu.sh
+++ dist/build-avr-gcc/build-avr-gcc-ubuntu.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 
-sudo apt-get install patch gcc wget scons libgmp-dev libmpfr-dev libgmp-dev libmpc-dev avrdude make
+sudo apt-get install patch gcc wget scons libgmp-dev libmpfr-dev libgmp-dev libmpc-dev avrdude make avr-gcc
 
 set -x
 set -e
